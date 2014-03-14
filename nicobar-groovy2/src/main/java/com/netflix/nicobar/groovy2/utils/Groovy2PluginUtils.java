2a20aa2f9e6a48fc84981440a724495c714ff628
Vasanth Asokan <vasokan@netflix.com>
2014-03-13 23:08:43 -0700

    Add nicobar-java7 subproject with support for java7 compiled bytecode archives

--- nicobar-groovy2/src/main/java/com/netflix/nicobar/groovy2/utils/Groovy2PluginUtils.java
+++ nicobar-groovy2/src/main/java/com/netflix/nicobar/groovy2/utils/Groovy2PluginUtils.java
@@ -6,17 +6,17 @@ import com.netflix.nicobar.core.plugin.ScriptCompilerPluginSpec;
 import com.netflix.nicobar.core.utils.ClassPathUtils;
 
 /**
- * Utility class for working with nicobar-groov2
+ * Utility class for working with nicobar-groovy2
  */
 public class Groovy2PluginUtils {
 
     private static final String NICOBAR_GROOVY2_PLUGIN_CLASS = "com.netflix.nicobar.groovy2.plugin.Groovy2CompilerPlugin";
-    private static final String NICOBAR_GROOVY2_LANG_NAME = "groovy2";
+    private static final String NICOBAR_GROOVY2_LANG_RUNTIME = "Groovy2Runtime";
     
     /**
      * Helper method to orchestrate commonly required setup of nicobar-groovy2, and return a groovy2 compiler spec.
      * @return a {@link ScriptCompilerPluginSpec} that is instantiated for the Groovy2 language, and is specified to 
-     *         depend on both the underyling groovy runtime, as well as nicobar-groov2.
+     *         depend on both the underyling groovy runtime, as well as nicobar-groovy2.
      */
     public static ScriptCompilerPluginSpec getCompilerSpec() {
         Path groovyRuntimePath = ClassPathUtils.findRootPathForResource("META-INF/groovy-release-info.properties", 
@@ -33,7 +33,7 @@ public class Groovy2PluginUtils {
         }
         
         // Create the compiler spec
-        ScriptCompilerPluginSpec compilerSpec = new ScriptCompilerPluginSpec.Builder(NICOBAR_GROOVY2_LANG_NAME)
+        ScriptCompilerPluginSpec compilerSpec = new ScriptCompilerPluginSpec.Builder(NICOBAR_GROOVY2_LANG_RUNTIME)
             .withPluginClassName(NICOBAR_GROOVY2_PLUGIN_CLASS)
             .addRuntimeResource(groovyRuntimePath)
             .addRuntimeResource(nicobarGroovyPluginPath)
